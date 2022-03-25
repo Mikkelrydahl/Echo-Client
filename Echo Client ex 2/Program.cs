b99d388b4a66059bfefaaff811483a127ec1a6c2
Mikkelrydahl <90452638+Mikkelrydahl@users.noreply.github.com>
2022-03-25 10:12:34 +0100

    Add files via upload

---
+++ Echo Client ex 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Echo_Client_ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client!");
+            TcpClient socket = new TcpClient("LocalHost", 7);
+
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+
+            string message = Console.ReadLine();
+            writer.WriteLine(message);
+            writer.Flush();
+
+            string receivemessage = reader.ReadLine();
+            Console.WriteLine("Sent" + receivemessage);
+        }
+
+
+    }
+}
